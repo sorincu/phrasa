49cdd2e02ba0d0cfeccdbd474f407e902da1c85e
chaotheo <scuibari1@gmail.com>
2022-05-30 21:02:29 +0100

    change background

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './css/App.module.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.background}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>HEADER</p>
       </header>
     </div>
   );
