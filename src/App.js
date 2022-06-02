2c0040df8cb92eced18d3e10b42ba5bcd1fa3e79
chaotheo <scuibari1@gmail.com>
2022-06-02 12:44:21 +0100

    Added buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import styles from './css/App.module.css';
+import styles from "./css/App.module.css";
+import Button from "@mui/material/Button";
 
 function App() {
   return (
-    <div className={styles.background}>
-      <header className="App-header">
-        <p>HEADER</p>
-      </header>
+    <div className={styles.buttonContainer}>
+      <Button color="info" variant="contained" size="large">
+        Import From Youtube
+      </Button>
+      
+      <Button color="info" variant="contained" size="large">
+        Audiofile
+      </Button>
     </div>
   );
 }
