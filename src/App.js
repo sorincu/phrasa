3eca55388700244ac79b9a9c84df76f687fc41fb
chaotheo <scuibari1@gmail.com>
2022-06-03 12:20:03 +0100

    Added navbar

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import Button from "@mui/material/Button";
 
 function App() {
   return (
-    <div className={styles.buttonContainer}>
-      <Button color="info" variant="contained" size="large">
-        Import From Youtube
-      </Button>
-      
-      <Button color="info" variant="contained" size="large">
-        Audiofile
-      </Button>
+    <div>
+      <div className={styles.bgNavbar}>
+        <div className={styles.navbar}>
+          <h2 className={styles.logo}>phrasa.</h2>
+        </div>
+      </div>
+
+      <div className={styles.buttonContainer}>
+        <Button color="info" variant="contained" size="large">
+          Import From Youtube
+        </Button>
+
+        <Button color="info" variant="contained" size="large">
+          Audiofile
+        </Button>
+      </div>
     </div>
   );
 }
