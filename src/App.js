96348e0c2a09004dc3aed8a55955493e348a32de
chaotheo <scuibari1@gmail.com>
2022-06-11 10:19:54 +0100

    Added navbar buttons

--- src/App.js
+++ src/App.js
@@ -7,14 +7,15 @@ function App() {
       <div className={styles.bgNavbar}>
         <div className={styles.navbar}>
           <h2 className={styles.logo}>phrasa.</h2>
+          <button className={styles.btn}>Buy me a coffee</button>
         </div>
       </div>
 
       <div className={styles.bannerContainer}>
         <h1 className={`${styles.banner} ${styles.primaryColor}`}>
-          Listen.&#160;
+          Listen&#160;
         </h1>
-        <h1 className={styles.banner}>Experience.&#160;</h1>
+        <h1 className={styles.banner}>closely.&#160;</h1>
         <h1 className={styles.banner}>Repeat.</h1>
       </div>
 
@@ -27,8 +28,8 @@ function App() {
       </div>
 
       <div className={styles.buttonContainer}>
-        <button className={styles.btn}>Import From Youtube</button>
-
+        <button className={styles.btn}>Import from Youtube</button>
+        <button className={styles.btn}>Import from Spotify</button>
         <button className={styles.btn}>Import Audiofile</button>
       </div>
     </div>
