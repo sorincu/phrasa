97c81d1edcac2a764cdff10360bc1929d5ca0143
chaotheo <scuibari1@gmail.com>
2022-06-20 20:00:21 +0100

    Displayed results from the API call

--- src/youtubeApi.js
+++ src/youtubeApi.js
@@ -5,7 +5,11 @@ const { google } = require('googleapis');
 google.youtube('v3').search.list({
     key: process.env.YOUTUBE_TOKEN,
     part: 'snippet',
-    q: 'joji',
+    q: 'aphex',
+    maxResults: 5,
 }).then((response) => {
-    console.log(response.data);
+    const { data } = response;
+    data.items.forEach((item) => {
+        console.log(`Title: ${item.snippet.title}\nDescription: ${item.snippet.description}\n`)
+    })
 }).catch((err) => console.log(err));
